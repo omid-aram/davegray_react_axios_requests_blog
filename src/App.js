d10c092a5e7f6c5d1ede42eb4622abe8a50e0c62
omid-aram <omid.aram@hotmail.com>
2023-01-01 16:05:02 +0330

    Using Axios

--- src/App.js
+++ src/App.js
@@ -5,38 +5,52 @@ import Nav from './Nav';
 import Home from './Home';
 import NewPost from './NewPost';
 import PostPage from './PostPage';
+import EditPost from './EditPost';
 import About from './About';
 import Missing from './Missing';
 import Footer from './Footer';
 import { format } from 'date-fns';
+import api from './api/posts';
 
 function App() {
-  const API_URL = "http://localhost:3500/posts";
+  //const API_URL = "http://localhost:3500/posts";
 
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
 
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch(API_URL);
-        if (!response.ok) throw Error("Please Reload the Page");
+        // const response = await fetch(API_URL);
+        // if (!response.ok) throw Error("Please Reload the Page");
+        // const result = await response.json();
+        // setPosts(result);
 
-        const result = await response.json();
-        setPosts(result);
+        const response = await api.get('/posts');
+        setPosts(response.data);
 
       } catch (err) {
-        console.log(err);
+        //console.log(err);
+
+        if (err.response) {
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
       }
     }
 
-    //fetchPosts();
-    (async () => await fetchPosts())();
+    //(async () => await fetchPosts())();
+    fetchPosts();
   }, []);
 
   useEffect(() => {
@@ -47,22 +61,46 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, datetime, title: postTitle, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigate("/");
+    try {
+      const response = await api.post('/posts', newPost);
+      const allPosts = [...posts, response.data /*newPost*/];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   }
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter(x => x.id !== id);
-    setPosts(postsList);
-    navigate("/");
+  const handleEdit = async (id) => {
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, datetime, title: editTitle, body: editBody };
+    try {
+      const response = await api.put(`/posts/${id}`, updatedPost);
+      setPosts(posts.map(x => x.id === id ? { ...response.data } : x));
+      setEditTitle('');
+      setEditBody('');
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`);
+      const postsList = posts.filter(x => x.id !== id);
+      setPosts(postsList);
+      navigate("/");
+    } catch (err) {
+      console.log(`Error: ${err.message}`);
+    }
   };
 
   return (
@@ -88,6 +126,16 @@ function App() {
           <PostPage
             posts={posts}
             handleDelete={handleDelete} />
+        } />
+        <Route path="/edit/:id" element={
+          <EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
+          />
         } />
         <Route path="/about" element={
           <About />
--- src/PostPage.js
+++ src/PostPage.js
@@ -12,6 +12,7 @@ const PostPage = ({ posts, handleDelete }) => {
                         <h2>{post.title}</h2>
                         <p className='postDate'>{post.datetime}</p>
                         <p className='postBody'>{post.body}</p>
+                        <Link to={`/edit/${post.id}`}><button className='editButton'>Edit Post</button></Link>
                         <button className='deleteButton' onClick={() => handleDelete(post.id)}>
                             Delete Post
                         </button>
@@ -19,11 +20,11 @@ const PostPage = ({ posts, handleDelete }) => {
                 }
                 {!post &&
                     <>
-                    <h2>Post Not Found</h2>
-                    <p>Well, that's disappointing.</p>
-                    <p>
-                        <Link to="/">Visit Our HomePage</Link>
-                    </p>
+                        <h2>Post Not Found</h2>
+                        <p>Well, that's disappointing.</p>
+                        <p>
+                            <Link to="/">Visit Our HomePage</Link>
+                        </p>
                     </>
                 }
             </article>
